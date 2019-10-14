9519875845ad3c4ff606b762d3a74a16a1ba6a03
dobryduch <michal.tyszko@gmail.com>
2019-10-14 12:34:05 +0200

    styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {CardList} from './components/card-list/card-list.component'
 import {SearchBox} from './components/search-box/searchBox.component'
-import "./App.css";
+import './App.css';
 
 class App extends Component {
   constructor() {
@@ -25,6 +25,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox placeholder='search monsters' handleSearch={this.handleSearch} />
         <CardList monsters={filteredMonsters}/>
       </div>
